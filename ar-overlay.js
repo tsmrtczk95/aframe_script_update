1f5e15e2d23bc8cfb743d88bee27bb0e4ece4125
Thomas <sempurai95@gmail.com>
2025-12-06 10:38:18 +0800

    Update ar-overlay.js

--- ar-overlay.js
+++ ar-overlay.js
@@ -1,1 +1,16 @@
+const modelViewer = document.getElementById("mainViewer");
+const overlay = document.getElementById("arOverlay");
+const exitAR = document.getElementById("exitAR");
 
+modelViewer.addEventListener("ar-status", (event) => {
+  if (event.detail.status === "session-started") {
+    overlay.classList.add("visible");
+  }
+  if (event.detail.status === "session-ended") {
+    overlay.classList.remove("visible");
+  }
+});
+
+exitAR.addEventListener("click", () => {
+  modelViewer.exitAR();
+});
